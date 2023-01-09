bae5683ddca9e22926f1890ad1c861cf0daf974d
Neikan <sladkov.e.m@outlook.com>
2023-01-09 10:36:14 +0300

    feat(project): add components

---
+++ text_field_styled/lib/components/app_screen_wrapper.dart
@@ -0,0 +1,27 @@
+// Flutter imports:
+import 'package:flutter/material.dart';
+
+// Project imports:
+import 'package:text_field_styled/components/app_drawer.dart';
+import 'package:text_field_styled/components/app_header.dart';
+
+class AppScreenWrapper extends StatelessWidget {
+  final Widget widget;
+
+  const AppScreenWrapper({
+    Key? key,
+    required this.widget,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      drawer: const AppDrawer(),
+      appBar: const AppHeader(),
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: widget,
+      ),
+    );
+  }
+}
